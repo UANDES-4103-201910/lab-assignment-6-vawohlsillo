866cda22c7cf7dae96fd0856604858e8b14247c0
vawohlsillo <vawohl@miuandes.cl>
2019-04-17 11:36:09 -0400

    helper methods in app controller create

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,21 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
+  helper_method :current_user, :is_user_logged_in?
   def index
 
   end
 
   def current_user
 	#complete this method
+	@current_user ||= User.find_by_id(session[:user])
   end
 
   def is_user_logged_in?
 	#complete this method
   	logged_in = false
+	if @current_user != nil
+		logged_in = true
+	end
 	if logged_in then true else redirect_to root_path end 
   end
 end
