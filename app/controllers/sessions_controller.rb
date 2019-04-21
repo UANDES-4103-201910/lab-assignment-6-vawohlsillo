1de129527a314a51beeece344b773ca227d592d2
vawohlsillo <vawohl@miuandes.cl>
2019-04-20 23:27:41 -0400

    ultimo intento

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,16 +5,25 @@ class SessionsController < ApplicationController
 
 	def create
 		#complete this method
-		user = User.authenticate(params[:email], params[:password])
+		user = User.find_by(email: params[:session][:email])
 		session[:current_user_id] = user.id
-		if is_user_logged_in?
-			flash[:success] = 'You have successfully logged in'
-			redirect_to root_url
+		if user && user.authenticate(params[:session][:password])		
+			if is_user_logged_in?
+				
+				log_in user
+				flash[:success] = 'You have successfully logged in'
+				redirect_to user
+			else
+				flash.now[:error] = 'Invalid email/password combination'
+				render 'new'
+			end
 		end
 	end
 
 	def destroy
 		#complete this method
+		log_out
+    		redirect_to root_url
 	end
 	
 end
